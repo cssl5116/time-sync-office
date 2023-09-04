16aacc5cf8c47b38836485a7d153a995f0f1ea0c
fishx <2690866872@qq.com>
2023-09-04 09:03:23 +0800

    提交index、checkIn、contacts、document、meetingList、mine初始化页面,完成小程序tabbar栏

---
+++ pages/checkIn/checkIn.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+	// 拒绝授权后再次请求
+	
+</script>
+
+<template>
+	<view>
+
+	</view>
+</template>
+
+<style scoped lang="less">
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,71 +1,200 @@
 <script setup>
-	import { storeToRefs } from 'pinia';
-	import { ref } from 'vue'
-	import { useUserStore } from "@/stores/user"
-
-	const modalName = ref('')
-	const title = ref('你好哇,再试试')
-	const userStore = useUserStore()
-	const {message} = storeToRefs(userStore)
-
-	function showOrHideModal(e) {
-		modalName.value = e
-	}
-
-	function login() {
-		uni.redirectTo({
-			url: '../login/login',
-		});
-	}
-
-	const getData = () => {
-		userStore.setUserInfo()
+	import { onMounted, ref } from 'vue'
+	import { toast, useRouter } from '@/utils/utils.js'
+	import { IMG_URL } from "@/server/config.js"
+	const unreadRows = ref(0)
+	onMounted(() => {
+		if (!uni.getStorageSync("token")) {
+			uni.showToast({
+				title: '请先登录',
+				icon: 'error'
+			})
+			// setTimeout(() => {
+			// 	uni.redirectTo({
+			// 		url: '/pages/login/login'
+			// 	})
+			// }, 800)
+		}
+	})
+	const toPage = (url) => {
+		if (!url) {
+			toast('正在施工中...敬请期待')
+			return
+		}
+		useRouter(url)
 	}
 </script>
 
 <template>
 	<view class="content">
-		<button @click="showOrHideModal('bottomModal')" class="cu-btn bg-grey lg">打开对话框</button>
-		<button @click="login" class="cu-btn bg-red margin-tb-sm lg">立即登录</button>
-		<view class="cu-modal bottom-modal" :class="modalName=='bottomModal'?'show':''">
-			<view class="cu-dialog" style="height: 40%;">
-				<view class="cu-bar bg-white">
-					<view class="action text-green" @click="getData">确定</view>
-					<view class="action text-blue" @tap="showOrHideModal('')">取消</view>
+		<swiper circular="true" interval="8000" duration="1000" class="swiper">
+			<swiper-item>
+				<image mode="widthFix" src="https://static-1258386385.cos.ap-beijing.myqcloud.com/img/banner/swiper-1.jpg">
+				</image>
+			</swiper-item>
+			<swiper-item>
+				<image mode="widthFix" src="https://static-1258386385.cos.ap-beijing.myqcloud.com/img/banner/swiper-2.jpg">
+				</image>
+			</swiper-item>
+			<swiper-item>
+				<image mode="widthFix" src="https://static-1258386385.cos.ap-beijing.myqcloud.com/img/banner/swiper-3.jpg">
+				</image>
+			</swiper-item>
+			<swiper-item>
+				<image mode="widthFix" src="https://static-1258386385.cos.ap-beijing.myqcloud.com/img/banner/swiper-4.jpg">
+				</image>
+			</swiper-item>
+			<swiper-item>
+				<image mode="widthFix" src="https://static-1258386385.cos.ap-beijing.myqcloud.com/img/banner/swiper-5.jpg">
+				</image>
+			</swiper-item>
+		</swiper>
+		<view class="notify-container">
+			<view class="notify-title">
+				<image :src="IMG_URL + 'icon-1.png'" mode="widthFix" class="notify-icon"></image>
+				消息提醒
+			</view>
+			<view class="notify-content">你有{{ unreadRows }}条未读消息</view>
+			<image :src="IMG_URL + 'icon-2.png'" mode="widthFix" class="more-icon"></image>
+		</view>
+		<view class="nav-container">
+			<view class="nav-row">
+				<view class="nav" @click="toPage('/pages/checkIn/checkIn')">
+					<image mode="widthFix" :src="IMG_URL+'nav-1.png'" class="icon"></image>
+					<text class="name">在线签到</text>
 				</view>
-				<view class="padding-xl">
-					{{ message }}
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-2.png'" mode="widthFix" class="icon"></image>
+					<text class="name">员工健康</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-3.png'" mode="widthFix" class="icon"></image>
+					<text class="name">在线请假</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-4.png'" mode="widthFix" class="icon"></image>
+					<text class="name">公务出差</text>
+				</view>
+			</view>
+			<view class="nav-row">
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-5.png'" mode="widthFix" class="icon"></image>
+					<text class="name">员工日报</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-6.png'" mode="widthFix" class="icon"></image>
+					<text class="name">我的加班</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-7.png'" mode="widthFix" class="icon"></image>
+					<text class="name">付款申请</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-8.png'" mode="widthFix" class="icon"></image>
+					<text class="name">费用报销</text>
+				</view>
+			</view>
+			<view class="nav-row">
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-9.png'" mode="widthFix" class="icon"></image>
+					<text class="name">公告通知</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-10.png'" mode="widthFix" class="icon"></image>
+					<text class="name">在线审批</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-11.png'" mode="widthFix" class="icon"></image>
+					<text class="name">物品领用</text>
+				</view>
+				<view class="nav" @click="toPage()">
+					<image :src="IMG_URL+'nav-12.png'" mode="widthFix" class="icon"></image>
+					<text class="name">采购申请</text>
 				</view>
 			</view>
 		</view>
 	</view>
 </template>
 
-<style>
+<style lang="less">
 	.content {
-		margin-top: 20px;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		background-color: #EEEEF4;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
+	.swiper {
+		height: 215px;
+	}
+
+	.swiper image {
+		width: 100% !important;
+	}
+
+	.notify-container {
+		background-color: #fff;
+		padding: 25px;
+		position: relative;
+		margin-bottom: 20rpx;
+	}
+
+	.notify-title {
+		font-size: 34rpx;
+		color: #3474FF;
+		margin-bottom: 10rpx;
+	}
+
+	.notify-icon {
+		width: 38rpx;
+		vertical-align: -8rpx;
+		margin-right: 5rpx;
+	}
+
+	.notify-content {
+		font-size: 30rpx;
+		color: #333;
+
+		&::before {
+			display: inline-block;
+			content: "";
+			width: 12rpx;
+			height: 12rpx;
+			border-radius: 6rpx;
+			background-color: red;
+			margin-left: 18rpx;
+			margin-right: 15rpx;
+			vertical-align: 3rpx;
+		}
+	}
+
+	.more-icon {
+		position: absolute;
+		right: 5rpx;
+		width: 60rpx;
+		top: 50rpx;
+	}
+
+	.nav-container {
+		background-color: #fff;
+		margin-bottom: 20rpx;
+		padding: 50rpx 40rpx;
+	}
+
+	.nav-row {
+		display: flex;
+		justify-content: space-between;
 		margin-bottom: 50rpx;
 	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.nav {
+		text-align: center;
 	}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.icon {
+		width: 80rpx;
+	}
+
+	.name {
+		display: block;
+		font-size: 30rpx;
+		color: #333;
 	}
 </style>
